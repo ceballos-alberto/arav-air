6746cf2873d0d9a3cd176eaed8fdca9d811aff5f
ceballos-alberto <albertoceb98@icloud.com>
2022-02-07 03:43:14 -0800

    Added AI model automatic download

---
+++ src/perception/model/variables/model.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------- #
+# ---------------- Environment Perception Module (EPM) - ARAV ---------------- #
+# ---------------------------------------------------------------------------- #
+# -------------------- Author : Alberto Ceballos Gonzalez -------------------- #
+# -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- #
+# --------- (c) Copyright 2022. Alberto Ceballos. All Rights Reserved -------- #
+# ---------------------------------------------------------------------------- #
+
+# Download object detection model #
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1ADjaCsya6D19ENnx5oPCDB0v_IHL7-bE' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1ADjaCsya6D19ENnx5oPCDB0v_IHL7-bE" -O variables.data-00000-of-00001 && rm -rf /tmp/cookies.txt
+
+# ---------------------------------------------------------------------------- #
+# ---------------- Environment Perception Module (EPM) - ARAV ---------------- #
+# ---------------------------------------------------------------------------- #
+# -------------------- Author : Alberto Ceballos Gonzalez -------------------- #
+# -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- #
+# --------- (c) Copyright 2022. Alberto Ceballos. All Rights Reserved -------- #
+# ---------------------------------------------------------------------------- #
+
