f5f77228771d5ecc4f27da4441bc61d0bf9a9ec2
ceballos-alberto <albertoceb98@icloud.com>
2022-02-09 09:39:53 -0800

    Added Gazebo models

---
+++ src/simulation/models/copy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------- #
+# ---------------------- Self-Driving Simulator - ARAV ----------------------- #
+# ---------------------------------------------------------------------------- #
+# -------------------- Author : Alberto Ceballos Gonzalez -------------------- #
+# -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- #
+# --------- (c) Copyright 2022. Alberto Ceballos. All Rights Reserved -------- #
+# ---------------------------------------------------------------------------- #
+
+# Copy Gazebo models to Gazebo default directory #
+
+cp -r hatchback_red/ ~/.gazebo/models/
+cp -r grey_wall/ ~/.gazebo/models/
+
+# ---------------------------------------------------------------------------- #
+# ---------------------- Self-Driving Simulator - ARAV ----------------------- #
+# ---------------------------------------------------------------------------- #
+# -------------------- Author : Alberto Ceballos Gonzalez -------------------- #
+# -------- E-mail : alberto.ceballos-gonzalez@student.isae-supaero.fr -------- #
+# --------- (c) Copyright 2022. Alberto Ceballos. All Rights Reserved -------- #
+# ---------------------------------------------------------------------------- #
+
